44e613ea19c1920765f9166c6728d818bc4de2f8
Eduardo Poleo <77074679+eduardopoleoflipp@users.noreply.github.com>
2025-04-25 09:59:26 -0400

    WIP

--- lib/utils.bash
+++ lib/utils.bash
@@ -34,45 +34,68 @@ list_all_versions() {
 }
 
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+  local version filename url
+  version="$1"
+  filename="$2"
 
-	# TODO: Adapt the release URL convention for eduardo-foo
-	url="$GH_REPO/archive/v${version}.tar.gz"
+  # Determine OS and architecture
+  local os arch
+  case "$(uname -s)" in
+    Darwin) os="darwin" ;;
+    Linux) os="linux" ;;
+    MINGW*|MSYS*|CYGWIN*) os="windows" ;;
+    *) fail "Unsupported operating system: $(uname -s)" ;;
+  esac
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  case "$(uname -m)" in
+    x86_64) arch="amd64" ;;
+    arm64|aarch64) arch="arm64" ;;
+    i*86) arch="386" ;;
+    *) fail "Unsupported architecture: $(uname -m)" ;;
+  esac
+
+  # Construct URL for the binary release
+  url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}_${version}_${os}_${arch}.tar.gz"
+
+  echo "* Downloading $TOOL_NAME release $version for ${os}_${arch}..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
+
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+  local install_type="$1"
+  local version="$2"
+  local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-
-		# TODO: Assert eduardo-foo executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+  (
+    mkdir -p "$install_path"
+    
+    # Extract the tar.gz file to the install path
+    echo "* Extracting $TOOL_NAME archive..."
+    tar -xzf "$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$version.tar.gz" -C "$install_path" --strip-components=1
+    
+    # Make sure the binary is executable
+    chmod +x "$install_path/$TOOL_NAME"
+    
+    # Verify installation
 
 		echo "##############"
 		echo "Download path $ASDF_DOWNLOAD_PATH"
 		echo "Install path $install_path"
 		echo "Tool command $tool_cmd"
+		echo "$install_path/$tool_cmd"
 		echo "##############"
 
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    test -x "$install_path/$TOOL_NAME" || fail "Expected $install_path/$TOOL_NAME to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
 }
+
